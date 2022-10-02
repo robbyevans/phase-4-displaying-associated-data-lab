bdfe58e58f3299baff2d8d88ddb2833daeb0aa0d
Evans Rutto <robbyevans001@gmail.com>
2022-10-02 14:00:38 +0300

    commit tree 11cbf9bff400047c7021277d01952c119bd4d953

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,12 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def index
+    items=Item.all
+    render json: items, include: :user
+  end
+
+  def render_not_found_response
+    render json: { error: "Item not found" }, status: :not_found
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+def show
+  users=User.find_by(id:params[:id])
+  render json: users,include: :items
+end
+
+def render_not_found_response
+  render json: { error: "User not found" }, status: :not_found
+end
 end
